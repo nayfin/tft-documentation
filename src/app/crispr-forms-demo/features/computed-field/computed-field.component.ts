7db7878f4d93ff8c3c5424d01f3e6c560a5f78a0
nayfin <natjkern@gmail.com>
2021-06-20 10:00:16 -0500

    release to stackblitz

--- src/app/crispr-forms-demo/features/computed-field/computed-field.component.ts
+++ src/app/crispr-forms-demo/features/computed-field/computed-field.component.ts
@@ -1,41 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ControlType, FormConfig } from '@tft/crispr-forms';
+import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'doc-computed-field',
   templateUrl: './computed-field.component.html',
   styleUrls: ['./computed-field.component.scss']
 })
-export class ComputedFieldComponent implements OnInit {
+export class ComputedFieldComponent {
   computedNumberFieldConfig: FormConfig = {
-    autocomplete: 'off',
     fields: [
       {
         controlType: ControlType.INPUT,
         inputType: 'number',
         controlName: 'factorA',
         label: 'This number is multiplied by the next value ',
+        autoComplete: 'off',
       },
       {
         controlType: ControlType.INPUT,
         inputType: 'number',
         controlName: 'factorB',
         label: 'Change me to see the next value change',
+        autoComplete: 'off',
       },
       {
         controlType: ControlType.INPUT,
         inputType: 'number',
         controlName: 'product',
         label: 'I am a computed value',
-        computeFieldConfig: {
-          controlNamesToWatch: ['factorA', 'factorB'],
-          computeCallback: (fieldValues: number[]) => {
-            console.log(fieldValues);
-            const [factorA, factorB] = fieldValues;
-            return factorA * factorB;
-          }
-        },
-        // computeField: computeValueFromFields
+        // // DEPRECATED: EXAMPLE OF old way of computing fields
+        // computeFieldConfig: {
+        //   controlNamesToWatch: ['factorA', 'factorB'],
+        //   computeCallback: (fieldValues: number[]) => {
+        //     console.log(fieldValues);
+        //     const [factorA, factorB] = fieldValues;
+        //     return factorA * factorB;
+        //   }
+        // },
+        computeValue: (group) => {
+          return combineLatest([
+            group.get('factorA').valueChanges,
+            group.get('factorB').valueChanges
+          ]).pipe(
+            map(([factorA, factorB]) => factorA * factorB)
+          )
+        }
       }
     ]
   }
@@ -47,32 +58,40 @@ export class ComputedFieldComponent implements OnInit {
         inputType: 'text',
         controlName: 'firstName',
         label: 'First Name',
+        spellcheck: true,
+        autoComplete: 'off',
+        autoCapitalize: 'off',
       },
       {
         controlType: ControlType.INPUT,
         inputType: 'text',
         controlName: 'lastName',
         label: 'Last Name',
+        autoComplete: 'off',
+        autoCapitalize: 'off'
       },
       {
         controlType: ControlType.INPUT,
         inputType: 'text',
         controlName: 'formattedName',
         label: 'Formatted Name',
-        computeFieldConfig: {
-          controlNamesToWatch: ['firstName', 'lastName'],
-          computeCallback: (values: string[]) => {
-            console.log({values})
-            return `${values[0] || ''} ${values[1] || ''}`.trim();
-          }
-        },
+        // // DEPRECATED: EXAMPLE OF old way of computing fields
+        // computeFieldConfig: {
+        //   controlNamesToWatch: ['firstName', 'lastName'],
+        //   computeCallback: (values: string[]) => {
+        //     console.log({values})
+        //     return `${values[0] || ''} ${values[1] || ''}`.trim();
+        //   }
+        // },
+        computeValue: (group) => {
+          return combineLatest([
+            group.get('firstName').valueChanges,
+            group.get('lastName').valueChanges
+          ]).pipe(
+            map(([firstName, lastName]) => `${firstName || ''} ${lastName || ''}`)
+          )
+        }
       }
     ]
   }
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
 }
